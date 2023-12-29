2d9b2ba03cbf492686948d1ae1550ce0d5b769bf
prosenjeetshil <prosenjeetshil@gmail.com>
2023-12-29 17:02:14 +0530

    feat: integrate API in ListContainer

---
+++ src/components/ListContainer.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState} from "react";
+import { fetchListData } from "../apiServices";
+
+const ListContainer = () => {
+    const [listData, setListData]= useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const data = await fetchListData();
+                setListData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setError(error);
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+        },[]);
+
+        return (
+            <>
+            <h1>hello world</h1>
+            </>
+        )
+};
+
+export default ListContainer
