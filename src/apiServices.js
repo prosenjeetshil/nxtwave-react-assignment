c1517c75d6eff5c12f22126bfd9fe078d32375a2
prosenjeetshil <prosenjeetshil@gmail.com>
2023-12-29 17:01:35 +0530

    feat: add apiServices for handling API requests

---
+++ src/apiServices.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const fetchListData = async () => {
+    try {
+        const response = await axios.get("https://apis.ccbp.in/list-creation/lists");
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { fetchListData };
